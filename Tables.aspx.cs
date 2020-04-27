b0427da7e0a9171281b069a4f04abb1f4358932f
jerrynott <jerrynott@users.noreply.github.com>
2020-04-27 21:35:43 +1000

    Update Tables.aspx.cs

--- Tables.aspx.cs
+++ Tables.aspx.cs
@@ -66,7 +66,9 @@ namespace TherapistPortal
             
                 userID = Global.GetIDFromWindowsLive(Request);
                 
-             Response.Write("<br/>jerry 02: " + StorageConnectionString.ToString());
+             Response.Write("<br/>jerry 02a: " + StorageConnectionString.ToString());
+             
+             Response.Write("<br/>jerry 02b: " + Environment.GetEnvironmentVariable("StorageAccount").ToString());
              
              Response.Write("<br/>jerry 03: " + Environment.GetEnvironmentVariable("TherapistPortalUserIDs").ToString());
              
