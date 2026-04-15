9da606151671cff2fe246e5729760af7fe456f24
ckeviny <ckeviny@gmail.com>
2026-04-15 15:44:25 +1000

    Fixed project files to account for new buttons

--- Tables.aspx.designer.cs
+++ Tables.aspx.designer.cs
@@ -67,6 +67,24 @@ namespace TherapistPortal {
         protected global::System.Web.UI.WebControls.Table tbl_TableDetailList;
         
         /// <summary>
+        /// btn_Dev control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Dev;
+
+        /// <summary>
+        /// btn_Prod control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Prod;
+
+        /// <summary>
         /// btn_ExportData control.
         /// </summary>
         /// <remarks>
